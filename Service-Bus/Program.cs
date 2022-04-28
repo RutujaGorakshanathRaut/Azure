550d04c81033d7d3b263b903d5e5a82adf310f58
RutujaGorakshanathRaut <87800851+RutujaGorakshanathRaut@users.noreply.github.com>
2022-04-28 12:16:48 +0530

    Add files via upload

---
+++ Service-Bus/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace QueueSender
+{
+    class Program
+    {
+        // connection string to your Service Bus namespace
+        static string connectionString = "Endpoint=sb://myservicebusrutu.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=wZ3eDRMnvzWVkauKxGwXQrSxJIeY5boAFI5O73F4KNs=";
+
+        // name of your Service Bus queue
+        static string queueName = "rutuqueue";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+
+        // number of messages to be sent to the queue
+        private const int numOfMessages = 5;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to the demo on implementing Service Bus Queue");
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+
+        }
+    }
+}
